297d746c07d9d905b76eab73e2d0ea01d8fc4b6f
Nicolas Ballier <nballier@free.fr>
2020-01-30 08:59:26 +0100

    Add files via upload
    A basic file to explore the "magick" package

---
+++ Image.R
@@ -0,0 +1,19 @@
+#FIRST STEPS : 
+  
+#Open Rstudio
+
+install.packages("magick")
+library(magick)
+
+#read the documentation :
+  
+https://ropensci.org/blog/2016/08/23/z-magick-release
+
+# copy and paste the script
+
+# blogs :
+#   https://www.r-bloggers.com/r-image-analysis-using-ebimage/
+#   
+#   https://www.r-bloggers.com/image-recognition-tutorial-in-r-using-deep-convolutional-neural-networks-mxnet-package/
+#   
+#   https://www.r-bloggers.com/image-recognition-in-r-using-convolutional-neural-networks-with-the-mxnet-package/
